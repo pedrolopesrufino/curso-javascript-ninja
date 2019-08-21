caf05c17fb5f39ddf2801fa14e47fe70804388fd
pedro <opedrolps@gmail.com>
2019-08-21 17:46:41 -0300

    add js file

--- challenge-27/challenge-27.js
+++ challenge-27/challenge-27.js
@@ -1,3 +1,6 @@
+(function(){
+  'use strict';
+
 /*
 Aproveitando a lib DOM que fizemos na semana anterior, crie agora para ela
 métodos semelhantes aos que existem no array, mas que sirvam para os
@@ -19,3 +22,43 @@ Crie os seguintes métodos para verificação de tipo:
 - isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
 O método isNull deve retornar `true` se o valor for null ou undefined.
 */
+
+
+function DOM(elements){
+  this.element = this.getDOMElements(elements)
+  
+}
+
+DOM.verify = function verify(argument){
+  return typeof(argument);
+}
+
+DOM.prototype.reduce((acc,item,index) =>{})
+DOM.prototype.reduceRight((acc,item,index) =>{}) 
+DOM.prototype.map((item,index) =>{})
+DOM.prototype.filter((item,index) =>{})
+DOM.prototype.every((item,index) =>{})
+DOM.prototype.some((item,index) =>{})
+
+
+//pega e gera elementos do dom
+DOM.prototype.getDOMElements = function getDOMElements(elements){
+  return document.querySelectorAll(elements);
+}
+
+DOM.prototype.on = function on(eventType, callback){
+  Array.prototype.forEach.call(this.element, function(element){
+    element.addEventListener(eventType,callback,false);
+})
+};
+DOM.prototype.off = function off(eventType,callback){
+  Array.prototype.forEach.call(this.element, function(element){
+    element.removeEventListener(eventType,callback,false);
+})
+};
+DOM.prototype.get = function get(){
+  return this.element
+};
+
+
+})();
