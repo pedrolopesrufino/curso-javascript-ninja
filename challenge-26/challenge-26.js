f07fa7a4d3fe081b362251571599a8d771175aba
pedro <opedrolps@gmail.com>
2019-08-22 09:39:21 -0300

    desafio concluido

--- challenge-26/challenge-26.js
+++ challenge-26/challenge-26.js
@@ -1,64 +1,79 @@
-(function(){
+(function () {
+  'use strict'
+  /*
+  O desafio dessa semana é criar uma mini library (biblioteca) para
+  reutilizarmos nossos códigos quando fizermos manipulação de DOM!
 
-/*
-O desafio dessa semana é criar uma mini library (biblioteca) para
-reutilizarmos nossos códigos quando fizermos manipulação de DOM!
-
-Requisitos:
-- O nome da lib deve ser "DOM".
-- Ela deve ser uma função construtora, que receberá uma string por parâmetro.
-
-
-Essa string será o nó do DOM a ser selecionado;
-- No construtor, você deve atribuir à `this.element` todos os elementos
-do DOM selecionados;
-- Extenda a lib para ter os métodos `on`, `off` e `get`.
-- O método `on` irá adicionar um listener de evento a todos os elementos
-selecionados.
-- O método `off` irá remover o listener de evento de todos os elementos
-selecionados.
-- O método `get` deve retornar os elementos.
-- O código abaixo deve funcionar corretamente após a lib criada.
-
-Dica: olhe os erros que acontecem no console, e vá resolvendo um a um.
-Só passe para o próximo problema quando tiver resolvido o anterior :)
-*/
-// ?
-
-function DOM(elements){
-  this.element = this.getDOMElements(elements)
-  
-}
-
-//pega e gera elementos do dom
-DOM.prototype.getDOMElements = function getDOMElements(elements){
-  return document.querySelectorAll(elements);
-}
-
-DOM.prototype.on = function on(eventType, callback){
-  Array.prototype.forEach.call(this.element, function(element){
-    element.addEventListener(eventType,callback,false);
-})
-};
-DOM.prototype.off = function off(eventType,callback){
-  Array.prototype.forEach.call(this.element, function(element){
-    element.removeEventListener(eventType,callback,false);
-})
-};
-DOM.prototype.get = function get(){
-  return this.element
-};
+  Requisitos:
+  - O nome da lib deve ser "DOM".
+  - Ela deve ser uma função construtora, que receberá uma string por parâmetro.
 
 
-var $a = new DOM('[data-js="link"]');
-$a.on('click', function handleClick(e) {
-  e.preventDefault();
-  console.log('clicou');
-  $a.off('click',handleClick)
-});
+  Essa string será o nó do DOM a ser selecionado;
+  - No construtor, você deve atribuir à `this.element` todos os elementos
+  do DOM selecionados;
+  - Extenda a lib para ter os métodos `on`, `off` e `get`.
+  - O método `on` irá adicionar um listener de evento a todos os elementos
+  selecionados.
+  - O método `off` irá remover o listener de evento de todos os elementos
+  selecionados.
+  - O método `get` deve retornar os elementos.
+  - O código abaixo deve funcionar corretamente após a lib criada.
 
-console.log('Elementos selecionados:', $a.get());
-console.log('Elementos selecionados:', $a.parentNode());
+  Dica: olhe os erros que acontecem no console, e vá resolvendo um a um.
+  Só passe para o próximo problema quando tiver resolvido o anterior :)
+  */
+  // ?
 
-console.log('$a é filho de body?',$a.get()[0].parentNode === document.body);
-})(); 
+  let key = false;
+  class DOM {
+    constructor(element) {
+      this.element = this.getDOMElements(element)
+    }
+
+  }
+
+  //pega e gera elementos do dom
+  DOM.prototype.getDOMElements = function getDOMElements(element) {
+    return document.querySelectorAll(element);
+  }
+
+  DOM.prototype.on = function on(eventType, callback) {
+    Array.prototype.forEach.call(this.element, function (element) {
+      element.addEventListener(eventType, callback, false);
+    })
+  };
+  DOM.prototype.off = function off(eventType, callback) {
+    Array.prototype.forEach.call(this.element, function (element) {
+      element.removeEventListener(eventType, callback, false);
+    })
+  };
+  DOM.prototype.get = function get() {
+    return this.element
+  };
+
+
+  var $a = new DOM('[data-js="link"]');
+
+  $a.on('click', function handleClick(e) {
+    e.preventDefault();
+    if(!key){
+      key = true;
+      console.log(`clicou em ${e.target}`);
+    }
+  });
+
+  $a.off('click', function unhandleClick(e) {
+    e.preventDefault();
+    if (key) {
+      key = false
+      console.log('desclicou')
+    };
+  })
+
+  console.log('Elementos selecionados:', $a.get());
+  // console.log('Elementos selecionados:', $a.parentNode());
+
+  console.log('$a é filho de body?', $a.get()[0].parentNode === document
+    .body);
+})();
