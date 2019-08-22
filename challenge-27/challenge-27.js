eb41f737a87cfc4afdce63950abd4a26e374b900
pedro <opedrolps@gmail.com>
2019-08-22 10:21:01 -0300

    add array methods in DOM library

--- challenge-27/challenge-27.js
+++ challenge-27/challenge-27.js
@@ -1,64 +1,85 @@
-(function(){
+(function () {
   'use strict';
 
-/*
-Aproveitando a lib DOM que fizemos na semana anterior, crie agora para ela
-métodos semelhantes aos que existem no array, mas que sirvam para os
-elementos do DOM selecionados.
-Crie os seguintes métodos:
-- forEach, map, filter, reduce, reduceRight, every e some.
+  /*
+  Aproveitando a lib DOM que fizemos na semana anterior, crie agora para ela
+  métodos semelhantes aos que existem no array, mas que sirvam para os
+  elementos do DOM selecionados.
+  Crie os seguintes métodos:
+  - forEach, map, filter, reduce, reduceRight, every e some.
 
-Crie também métodos que verificam o tipo do objeto passado por parâmetro.
-Esses métodos não precisam depender de criar um novo elmento do DOM, podem
-ser métodos estáticos.
+  Crie também métodos que verificam o tipo do objeto passado por parâmetro.
+  Esses métodos não precisam depender de criar um novo elmento do DOM, podem
+  ser métodos estáticos.
 
-Métodos estáticos não obrigam o uso do `new`, podendo ser usados diretamente
-no objeto, como nos exemplos abaixo:
-DOM.isArray([1, 2, 3]); // true
-DOM.isFunction(function() {}); // true
-DOM.isNumber('numero'); // false
+  Métodos estáticos não obrigam o uso do `new`, podendo ser usados diretamente
+  no objeto, como nos exemplos abaixo:
+  DOM.isArray([1, 2, 3]); // true
+  DOM.isFunction(function() {}); // true
+  DOM.isNumber('numero'); // false
 
-Crie os seguintes métodos para verificação de tipo:
-- isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
-O método isNull deve retornar `true` se o valor for null ou undefined.
-*/
+  Crie os seguintes métodos para verificação de tipo:
+  - isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
+  O método isNull deve retornar `true` se o valor for null ou undefined.
+  */
+  const $a = new DOM('[data-js="link"]')
+  console.log($a)
 
+  function DOM(element) {
+    this.element = this.getDOMElements(element)
+  }
 
-function DOM(elements){
-  this.element = this.getDOMElements(elements)
-  
-}
-
-DOM.verify = function verify(argument){
-  return typeof(argument);
-}
-
-DOM.prototype.reduce((acc,item,index) =>{})
-DOM.prototype.reduceRight((acc,item,index) =>{}) 
-DOM.prototype.map((item,index) =>{})
-DOM.prototype.filter((item,index) =>{})
-DOM.prototype.every((item,index) =>{})
-DOM.prototype.some((item,index) =>{})
+  DOM.verify = function verify(argument) {
+    return typeof (argument);
+  }
 
 
-//pega e gera elementos do dom
-DOM.prototype.getDOMElements = function getDOMElements(elements){
-  return document.querySelectorAll(elements);
-}
-
-DOM.prototype.on = function on(eventType, callback){
-  Array.prototype.forEach.call(this.element, function(element){
-    element.addEventListener(eventType,callback,false);
-})
-};
-DOM.prototype.off = function off(eventType,callback){
-  Array.prototype.forEach.call(this.element, function(element){
-    element.removeEventListener(eventType,callback,false);
-})
-};
-DOM.prototype.get = function get(){
-  return this.element
-};
+  DOM.prototype.forEach = function forEach() {
+    return Array.prototype.forEach.apply(this.element, arguments)
+  }
+  DOM.prototype.map = function map() {
+    return Array.prototype.map.apply(this.element, arguments)
+  }
+  DOM.prototype.some = function some() {
+    return Array.prototype.some.apply(this.element, arguments)
+  }
+  DOM.prototype.every = function every() {
+    return Array.prototype.every.apply(this.element, arguments)
+  }
+  DOM.prototype.filter = function filter() {
+    return Array.prototype.filter.apply(this.element, arguments)
+  }
 
 
+  DOM.prototype.reduce = function reduce() {
+    return Array.prototype.reduce.apply(this.element, arguments)
+  }
+
+
+  DOM.prototype.reduceRight = function reduceRight() {
+    return Array.prototype.reduceRight.apply(this.element, arguments)
+  }
+  //pega e gera elementos do dom
+  DOM.prototype.getDOMElements = function getDOMElements(element) {
+    return document.querySelectorAll(element);
+  }
+
+  DOM.prototype.on = function on(eventType, callback) {
+    Array.prototype.forEach.call(this.element, function (element) {
+      element.addEventListener(eventType, callback, false);
+    })
+  };
+  DOM.prototype.off = function off(eventType, callback) {
+    Array.prototype.forEach.call(this.element, function (element) {
+      element.removeEventListener(eventType, callback, false);
+    })
+  };
+  DOM.prototype.get = function get() {
+    return this.element
+  };
+
+
+  $a.forEach(function (item) {
+    console.log('selecionados ' + item.firstChild.nodeValue)
+  })
 })();
